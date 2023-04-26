f2c2a5e3e71a018073dce6a7dbd8f006e31383db
shantdashjian <shantgdashjian@gmail.com>
2023-04-26 23:17:46 +0400

    Add color scheme generation ability and the stretch goal of copying color hex value on click

---
+++ index.js
@@ -0,0 +1,48 @@
+import modes from './modes'
+
+const colorPicker = document.getElementById('color-picker')
+const modeSelector = document.getElementById('mode-selector')
+const getColorSchemeBtn = document.getElementById('get-color-scheme-btn')
+const colorScheme = document.getElementById('color-scheme')
+
+renderModes()
+generateColorScheme()
+
+function renderModes() {
+		modeSelector.innerHTML = modes.map(mode =>
+			`<option value="${mode}">${mode}</option>`).join('')
+}
+
+getColorSchemeBtn.addEventListener('click', generateColorScheme)
+
+function generateColorScheme() {
+	const hex = colorPicker.value.substring(1)
+	const mode = modeSelector.value
+	const baseURL = 'https://www.thecolorapi.com'
+	const endpoint = '/scheme'
+	fetch(`${baseURL}${endpoint}?hex=${hex}&mode=${mode}`)
+		.then(res => res.json())
+		.then(data => {
+			colorScheme.innerHTML = data.colors.map(color => {
+				const colorHexValue = color.hex.value
+				return `
+					<div class="color-block flex" onclick="copyColorHexValue('${colorHexValue}')">
+						<div class="color" style="background-color: ${colorHexValue}">
+						</div>
+						<div class="color-value flex">
+							<p>${colorHexValue}</p>
+						</div>
+					</div>
+				`
+			}).join('')
+		})
+}
+
+/*
+ I defined the function in the global scope using "window" so that it's accessible
+ by the onclick attribute. This is because I made index.js of type="module" which
+ puts all its functions in a separate scope of their own, and not in the global scope.
+ */
+window.copyColorHexValue = function(colorHexValue) {
+	navigator.clipboard.writeText(colorHexValue)
+}
---
+++ modes.js
@@ -0,0 +1,12 @@
+const modes = [
+	'monochrome',
+	'monochrome-dark',
+	'monochrome-light',
+	'analogic',
+	'complement',
+	'analogic-complement',
+	'triad',
+	'qua'
+]
+
+export default modes;
